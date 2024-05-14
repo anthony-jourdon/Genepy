39c565ab85fe058cec452fec5614c9ecf5c50207
Anthony Jourdon <jourdon_anthony@hotmail.fr>
2024-05-14 20:08:34 +0200

    Add a coor=None attribute to entry parameters of the class Domain. This way, it is possible to pass an already created coor object. Change the __str__() method to print the content of the class attributes using their actual name as an indication for the user

--- bcpy/domain.py
+++ bcpy/domain.py
@@ -3,7 +3,7 @@ import numpy as np
 import sympy as sp
 
 class Domain:
-  def __init__(self,dim:int,minCoor:np.ndarray,maxCoor:np.ndarray,size:np.ndarray) -> None:
+  def __init__(self,dim:int,minCoor:np.ndarray,maxCoor:np.ndarray,size:np.ndarray,coor=None) -> None:
     self.dim   = dim
     
     if minCoor.shape[0] != self.dim:
@@ -17,14 +17,21 @@ class Domain:
     self.L     = maxCoor
     self.n     = size
 
-    self.num_coor = None
+    self.num_coor = coor
     self.sym_coor = None
-
-    self.numerical_coordinates()
+    if self.num_coor is None:
+      self.numerical_coordinates()
     self.symbolic_coordinates()
 
   def __str__(self) -> str:
-    return f'Domain: {self.dim}D\nOrigin: {self.O}\nLength: {self.L}\nSize: {self.n}'
+    attributes = vars(self)
+    s = f'Domain:\n'
+    for attribute in attributes:
+      if attribute == 'num_coor':
+        s += f'\t{attribute}.shape:\t{attributes[attribute][0].shape}\n'
+      else:
+        s += f'\t{attribute}:\t{attributes[attribute]}\n'
+    return s
 
   def numerical_coordinates_2d(self):
     X,Z = np.meshgrid(np.linspace(self.O[0],self.L[0],self.n[0], dtype=np.float64),
@@ -64,34 +71,16 @@ class Domain:
 def test2d():
   O = np.array([-0.5, -0.5], dtype=np.float64)
   L = np.array([0.5, 0.5], dtype=np.float64)
-  n = np.array([4,4], dtype=np.int32)
+  n = np.array([4,3], dtype=np.int32)
   dom = Domain(2,O,L,n)
   print(dom)
-  print('X:',dom.num_coor[0].shape,'\n',dom.num_coor[0])
-  print('Z:',dom.num_coor[1].shape,'\n',dom.num_coor[1])
-  print(dom.sym_coor)
-  coor = np.zeros(shape=(dom.n[0]*dom.n[1],2), dtype=np.float64)
-  coor[:,0] = dom.num_coor[0].reshape(n[1]*n[0],order='F')
-  coor[:,1] = dom.num_coor[1].reshape(n[1]*n[0],order='F')
-  print(coor)
-
+  
 def test3d():
   O = np.array([-0.5, -0.5, -0.5], dtype=np.float64)
   L = np.array([0.5, 0.5, 0.5], dtype=np.float64)
-  n = np.array([4,4,4], dtype=np.int32)
+  n = np.array([4,5,3], dtype=np.int32)
   dom = Domain(3,O,L,n)
   print(dom)
-  print('X:',dom.num_coor[0].shape,'\n',dom.num_coor[0])
-  print('Y:',dom.num_coor[1].shape,'\n',dom.num_coor[1])
-  print('Z:',dom.num_coor[2].shape,'\n',dom.num_coor[2])
-  print(dom.sym_coor)
-
-  coor = np.zeros(shape=(dom.n[0]*dom.n[1]*dom.n[2],3), dtype=np.float64)
-  coor[:,0] = dom.num_coor[0].reshape(n[2]*n[1]*n[0],order='F')
-  coor[:,1] = dom.num_coor[1].reshape(n[2]*n[1]*n[0],order='F')
-  coor[:,2] = dom.num_coor[2].reshape(n[2]*n[1]*n[0],order='F')
-  print(coor)
-
 
 if __name__ == "__main__":
   print("Running tests")
